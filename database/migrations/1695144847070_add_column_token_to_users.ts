c03ccf32dd286ba835f62d36aaf9bd469d0b98b6
alicematins7 <martinsalice280206@gmail.com>
2023-09-19 14:38:16 -0300

    alice

---
+++ database/migrations/1695144847070_add_column_token_to_users.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+  schema: any
+
+  public async up () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('token')
+    })
+  }
+
+  public async down () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('token')
+    })
+  }
+}
