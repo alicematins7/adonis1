0d16ca29cc1a360fa991191cc7ca4593ab27e344
alicematins7 <martinsalice280206@gmail.com>
2023-09-25 10:06:15 -0300

    data

--- start/routes.ts
+++ start/routes.ts
@@ -27,5 +27,5 @@ Route.get('/', async () => {
 Route.post("/register", "AdonisJsController.register")
 Route.post("/login", "AdonisJsController.login")
 Route.group(() => {
-  Route.put("/user", "UsersCrontroller.update")
+  Route.put("/user", "UsersController.update")
 }).middleware('auth')
